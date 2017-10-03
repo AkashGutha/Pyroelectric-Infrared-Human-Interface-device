1a7cb4a1122678199f8d74afd7938ed1ce187bb6
Akash Gutha <akash.gutha@outlook.com>
2017-10-03 22:33:46 +0530

    remove serial integration

--- Keyboard_basic/Keyboard_basic.ino
+++ Keyboard_basic/Keyboard_basic.ino
@@ -4,11 +4,6 @@ void setup()
 {
   // put your setup code here, to run once:
   Keyboard.begin();
-  Serial.begin(38400);
-  while (!Serial)
-  {
-    ; // wait for serial port to connect. Needed for native USB port only
-  }
 }
 
 void loop()
@@ -23,15 +18,10 @@ void TypeString(String string)
   // find the length of the input string
   int length = string.length();
 
-  // print out the string length
-  Serial.println("");
-  Serial.println("the length is : " + String(length));
-
   // send in the keystrokes
   for (int i = 0; i < length; i++)
   {
     KeyPress(string[i]);
-    Serial.print(string[i]);
   }
 }
 
