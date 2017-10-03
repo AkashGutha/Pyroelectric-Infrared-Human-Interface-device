a4dac4b06d1e6f0467d18b05e31710a3a22ceddd
Akash Gutha <akash.gutha@outlook.com>
2017-10-03 22:33:58 +0530

    added HID basic exmaple with PIR

---
+++ PIR_HID/PIR_HID.ino
@@ -0,0 +1,18 @@
+// include the keyboard library
+#include <Keyboard.h>
+
+// custom INPUT pin settings
+#define PIR_PIN 7
+#define LED_PIN 13
+
+// variables for reading
+float reading = 0;
+
+void setup(){
+  // start Keyboard
+  Keyboard.begin();
+}
+
+void loop(){
+  
+}
